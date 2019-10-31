771ead541bdf2444a5ebba2b98d33e993090863b
余昌泓 <358116732@qq.com>
2019-10-31 23:30:10 +0800

    优化网格信息页面样式

--- src/pages/grid_info/index.vue
+++ src/pages/grid_info/index.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="jiance-wrap">
-    <HeaderBar title="微网格" titleColor="#fff" iconColor="#fff" :iconLeft="true" backgroundColor="#00ACFF" :isBack="true" @clickEvent="handleClickHeader"></HeaderBar>
+    <HeaderBar title="网格区域" titleColor="#fff" iconColor="#fff" :iconLeft="true" backgroundColor="#0187FB" :isBack="true" @clickEvent="handleClickHeader"></HeaderBar>
     <div class="page-wrap" :style="{paddingTop: statusBarHeight + 'px'}">
-      <div class="page-title">
-        <img src="../../../static/logo.jpg"/>
-        <div class="grid-title">{{gridTitle}}</div>
-        <div class="grid-number">{{"网格编号：" + gridNumber}}</div>
+      <div class="grid-block">
+        <div class="page-title">
+          <div class="grid-title">当前网格区域</div>
+          <div class="grid-info">
+            <img src="../../../static/logo.jpg"/>
+            <div>
+              <h3>{{gridTitle}}</h3>
+              <h4>{{"网格编号：" + gridNumber}}</h4>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="submit_btn" @click="newWorkOrder()">新建工作单</div>
+      <p>发现隐患？点击按钮上报隐患</p>
+      <div class="submit_btn" @click="newWorkOrder()">上报隐患</div>
     </div>
   </div>
 </template>
